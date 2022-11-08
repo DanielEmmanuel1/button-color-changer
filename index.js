7c923b5f48ca2efdcf04112f0c01b878e6b5ede9
Crypt.sol.codes <110722148+DanielEmmanuel1@users.noreply.github.com>
2022-11-08 14:06:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let index = 0;
 
 function changeColors() {
     let colors = ["yellow", "red", "green", "indigo", "blue"];
-    document.getElementsByTagName("body")[0].style.background = colors[index++];
+    document.getElementsByTagName("body")[0,4].style.background = colors[index++];
 
     if (index > colors.length - 1);
     index = 0;
